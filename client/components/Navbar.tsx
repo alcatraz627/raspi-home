7305252ef111a06d179432dd2d70976ab64baa56
Aakarsh Chopra <aakarsh@startree.ai>
2023-12-06 12:51:48 +0530

    show all routes on navbar

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material"
-
+import React from "react";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { RouteMap } from "../routes";
 
 export const Navbar = (): JSX.Element => {
-
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <Typography variant="h6" component="div" sx={{
-                        flexGrow: 1,
-                    }}>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{
+                            flexGrow: 1,
+                        }}
+                    >
                         Chopra Ras-pi Home
                     </Typography>
-                    <Button variant="text" color="inherit">Button</Button>
+                    {Object.values(RouteMap).map((route) => (
+                        <Button
+                            key={route.key}
+                            variant="text"
+                            color="inherit"
+                            href={route.path}
+                            sx={{
+                                textTransform: "capitalize"
+                            }}
+                        >
+                            {route.key}
+                        </Button>
+                    ))}
                 </Toolbar>
             </AppBar>
-        </Box>)
-}
+        </Box>
+    );
+};
