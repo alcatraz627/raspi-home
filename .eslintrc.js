1e164bd6be08b27fecf9a1130013180e9df2c846
Aakarsh Chopra <aakarsh@startree.ai>
2023-12-06 13:48:46 +0530

    attempted eslint linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,26 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:react-hooks/recommended",
-        "prettier"
+        "prettier",
     ],
     ignorePatterns: ["dist", ".eslintrc.cjs"],
     parser: "@typescript-eslint/parser",
     plugins: ["react-refresh", "react-hooks"],
     rules: {
+        "no-unused-vars": "on",
+        "unused-imports/no-unused-imports": "error",
+        "unused-imports/no-unused-vars": [
+            "warn",
+            {
+                vars: "all",
+                varsIgnorePattern: "^_",
+                args: "after-used",
+                argsIgnorePattern: "^_",
+            },
+        ],
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true },
         ],
     },
-}
+};
