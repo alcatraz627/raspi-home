950de7ff09a11deb8102f09985822403bffdd187
Aakarsh Chopra <aakarsh@startree.ai>
2024-01-30 12:14:53 +0530

    server run change

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,20 +6,24 @@ import ViteExpress from "vite-express";
 // import "module-alias/register";
 
 const PORT = 3000;
-const HOST = "0.0.0.0";
+const HOST = "localhost";
 
 const initServer = async () => {
-    const server = express();
-    server.use(cors());
+    const app = express();
+    app.use("*", cors());
 
-    renderStaticAssets(server);
+    renderStaticAssets(app);
 
-    server.use("/api", apiRouter);
+    app.use("/api", apiRouter);
 
     // TODO: Server flag
-    ViteExpress.listen(server, PORT, () => {
+    const server = app.listen(PORT, () => {
         console.log(`Server is listening on http://${HOST}:${PORT}`);
     });
+
+    ViteExpress.config({ viteConfigFile: "vite.config.ts" });
+
+    ViteExpress.bind(app, server);
 };
 
 initServer();
